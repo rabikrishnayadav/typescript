3c16a07a4e9a93702849eaebe1103bb6c9b9d303
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-22 22:36:28 +0545

    type assertion in TS

--- types.ts
+++ types.ts
@@ -1,1 +1,9 @@
 console.log("welcome to learn typscript");
+
+let message; // here is only declare the variable not its type
+message = "Assertion with angle-bracket";
+(<string>message).length;  // assertion with angle-bracket
+
+let msg; // here is only declare the variable not its type
+msg = "Assertion use as a syntax";
+(msg as string).length; // assertion with syntax
