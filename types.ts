f03490771fd73f70a5ebacd32c17d5d42e6064c6
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-22 22:47:54 +0545

    Arrow Function

--- types.ts
+++ types.ts
@@ -1,5 +1,7 @@
 console.log("welcome to learn typscript");
 
+// assertion in typescript
+
 let message; // here is only declare the variable not its type
 message = "Assertion with angle-bracket";
 (<string>message).length;  // assertion with angle-bracket
@@ -7,3 +9,9 @@ message = "Assertion with angle-bracket";
 let msg; // here is only declare the variable not its type
 msg = "Assertion use as a syntax";
 (msg as string).length; // assertion with syntax
+
+// Arrow function in type script
+
+let myFun = (value) =>{
+    console.log(value);
+}
