ca08d44c49581b899abb8c6b37e87f2e20b65b0b
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-23 09:46:02 +0545

    classes in TS

---
+++ class.ts
@@ -0,0 +1,25 @@
+// cohesive
+
+class User{
+    // declare the frilds
+    name:string;
+    age: number;
+    address: string;
+
+    // this is called method because this function is inside the class
+    getName(){  // declared the method
+
+    }
+
+    // delcare the method for age
+    getAge(){
+
+    }
+
+    // declare the method for address
+    getAddress(){
+
+    }
+}
+
+// getAddresss(){} // this is a function because of this is declared from outside the class
